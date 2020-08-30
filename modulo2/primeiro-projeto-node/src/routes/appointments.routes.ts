f96644cf45097c0d7ecd1127e7a979e424d30cac
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-08-30 15:28:12 -0300

    disruption files

--- modulo2/primeiro-projeto-node/src/routes/appointments.routes.ts
+++ modulo2/primeiro-projeto-node/src/routes/appointments.routes.ts
@@ -1,20 +1,36 @@
 import { Router, Request, Response } from 'express';
-import { uuid } from 'uuidv4';
+import { startOfHour, parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
-const appointments = [];
+appointmentsRouter.get('/', (req: Request, res: Response) => {
+  const appointments = appointmentsRepository.all();
+
+  return res.json(appointments);
+});
 
 appointmentsRouter.post('/', (req: Request, res: Response) => {
   const { provider, date } = req.body;
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date,
-  };
+  const parsedDate = startOfHour(parseISO(date));
 
-  appointments.push(appointment);
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate,
+  );
+
+  if (findAppointmentInSameDate) {
+    return res
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate,
+  });
 
   return res.json(appointment);
 });
