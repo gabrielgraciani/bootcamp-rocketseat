2d2b6f807b6c915db21da79843930c5255a31e28
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-09-03 20:58:44 -0300

    added jwt and authenticated routes

---
+++ modulo2/primeiro-projeto-node/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '060a442d4007f56035024e6fccfa1e1e',
+    expiresIn: 'id',
+  },
+};
--- modulo2/primeiro-projeto-node/src/routes/appointments.routes.ts
+++ modulo2/primeiro-projeto-node/src/routes/appointments.routes.ts
@@ -5,7 +5,11 @@ import { parseISO } from 'date-fns';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
 const appointmentsRouter = Router();
+
+appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.get('/', async (req: Request, res: Response) => {
   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
