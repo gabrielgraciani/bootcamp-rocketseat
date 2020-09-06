4993e33f7a41ed2b9aebe343f2cb29839f349874
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-09-06 11:56:10 -0300

    initial commit gobarber web

---
+++ gobarber/web/src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <span>qwewq eqwweq</span>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default App;
