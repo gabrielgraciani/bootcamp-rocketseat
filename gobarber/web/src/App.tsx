52f867282d0d94ca83ff9ee0d7b3ffc8e4e17017
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-09-06 19:12:02 -0300

    used useContext to make the system authentication

--- gobarber/web/src/App.tsx
+++ gobarber/web/src/App.tsx
@@ -3,11 +3,15 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 import SignIn from './pages/Signin';
 
+import { AuthProvider } from './context/AuthContext';
+
 const App: React.FC = () => {
   return (
     <>
       <GlobalStyle />
-      <SignIn />
+      <AuthProvider>
+        <SignIn />
+      </AuthProvider>
     </>
   );
 };
