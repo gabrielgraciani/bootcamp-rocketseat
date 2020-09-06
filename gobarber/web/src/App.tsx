a6194e789fcb6afe855f1f4095e75e3d0aaf2021
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-09-06 15:42:44 -0300

    added unform

--- gobarber/web/src/App.tsx
+++ gobarber/web/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 import GlobalStyle from './styles/global';
-import SignIn from './pages/Signin';
+// import SignIn from './pages/Signin';
+import SignUp from './pages/Signup';
 
 const App: React.FC = () => {
   return (
     <>
       <GlobalStyle />
-      <SignIn />
+      {/* <SignIn /> */}
+      <SignUp />
     </>
   );
 };
---
+++ gobarber/web/src/pages/Signup/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Container, Content, Background } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+const SignUp: React.FC = () => {
+  function handleSubmit(data: any): void {
+    console.log('data', data);
+  }
+  return (
+    <Container>
+      <Background />
+
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+
+          <Input type="text" icon={FiUser} name="name" placeholder="Nome" />
+          <Input type="text" icon={FiMail} name="email" placeholder="E-mail" />
+          <Input
+            type="password"
+            icon={FiLock}
+            name="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit"> Cadastrar</Button>
+        </Form>
+
+        <a href="create">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+export default SignUp;
