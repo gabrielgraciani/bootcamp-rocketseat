e8a2099bd9f52bdfaa779e4adebc39e5e0814a3f
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-01-02 13:26:46 -0300

    created some routes of application mobile

--- gobarber/appgobarber/src/App.tsx
+++ gobarber/appgobarber/src/App.tsx
@@ -1,6 +1,18 @@
-import React from "react";
-import { View } from "react-native";
+import 'react-native-gesture-handler';
 
-const App = (): React.ReactElement => <View />;
+import React from 'react';
+import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './routes';
+
+const App = (): React.ReactElement => (
+  <NavigationContainer>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <View style={{ flex: 1 }}>
+      <Routes />
+    </View>
+  </NavigationContainer>
+);
 
 export default App;
---
+++ gobarber/appgobarber/src/pages/SignIn/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const SignIn = (): React.ReactElement => <Container />;
+
+export default SignIn;
